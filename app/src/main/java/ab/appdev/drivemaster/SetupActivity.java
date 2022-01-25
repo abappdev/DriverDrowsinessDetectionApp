f8b4d1837e76bdd71df2e01fa5630e7c046c665f
ABHISHEK PRALHAD BHALERAO <abhishekbhalerao.appdev@gmail.com>
2022-01-25 12:16:17 +0530

    Sensitivity

--- app/src/main/java/ab/appdev/drivemaster/SetupActivity.java
+++ app/src/main/java/ab/appdev/drivemaster/SetupActivity.java
@@ -33,26 +33,20 @@ public class SetupActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 sharedpreferences.edit().putString(Configurable.SENSITIVITY, progress + "").apply();
 
-
                 if (0 <= progress & progress <= 3) {
                     //sensitivityBar.color
                     sensitivityShowingLabel.setText("LOWER IS MORE SAFE");
                     sensitivityShowingLabel.setBackgroundColor(Color.GREEN);
                     sensitivityShowingLabel.setTextColor(Color.BLACK);
-
                 } else if (4 <= progress && progress <= 8) {
                     sensitivityShowingLabel.setText("LOOKING SAFE FOR YOU");
                     sensitivityShowingLabel.setBackgroundColor(Color.YELLOW);
                     sensitivityShowingLabel.setTextColor(Color.BLACK);
-
-
                 } else {
                     sensitivityShowingLabel.setText("NO SAFETY AT ALL");
                     sensitivityShowingLabel.setBackgroundColor(Color.RED);
                     sensitivityShowingLabel.setTextColor(Color.WHITE);
-
                 }
-
             }
 
             @Override
