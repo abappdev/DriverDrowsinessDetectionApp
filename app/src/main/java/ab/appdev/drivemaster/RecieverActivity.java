e827b28ae1d4d08097a808497e95713f1a99e492
ABHISHEK PRALHAD BHALERAO <abhishekbhalerao.appdev@gmail.com>
2022-03-03 17:57:27 +0530

    QR

--- app/src/main/java/ab/appdev/drivemaster/RecieverActivity.java
+++ app/src/main/java/ab/appdev/drivemaster/RecieverActivity.java
@@ -3,8 +3,10 @@ package ab.appdev.drivemaster;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +18,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
+
+import ab.appdev.drivemaster.facedetection.DriverFaceDetection;
 
 public class RecieverActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
@@ -49,10 +53,11 @@ public class RecieverActivity extends AppCompatActivity {
 
                 // after getting the value we are setting
                 // our value to our text view in below line.
-                info.setText(Objects.requireNonNull(snapshot.child("Safe").getValue()).toString());
+                info.setText(Objects.requireNonNull(snapshot.child("INFO").getValue()).toString());
 
-                if(Objects.requireNonNull(snapshot.child("INFO").getValue()).toString().equals("000000000000")){
-                    info.setText(Objects.requireNonNull(snapshot.child("WARNING").getValue()).toString());
+                if(Objects.requireNonNull(snapshot.child("INFO").getValue()).toString().equals("00000000000000")){
+                    info.setText("WARNING");
+                alert_box();
                 }
 
             }
@@ -64,6 +69,47 @@ public class RecieverActivity extends AppCompatActivity {
                 Toast.makeText(RecieverActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
             }
         });
+
+
+    }
+
+
+    public void play_media() {
+        stop_playing();
+        mp = MediaPlayer.create(this, R.raw.alarm);
+        mp.start();
+    }
+
+    public void stop_playing() {
+        if (mp != null) {
+            mp.stop();
+            mp.release();
+            mp = null;
+        }
+    }
+
+    private MediaPlayer mp;
+
+    public void alert_box() {
+        play_media();
+        runOnUiThread(() -> {
+            play_media();
+
+            AlertDialog dig;
+            dig = new AlertDialog.Builder(RecieverActivity.this)
+                    .setTitle("Drowsy Alert !!!")
+                    .setMessage("Tracker suspects that the driver is experiencing Drowsiness")
+                    .setPositiveButton(android.R.string.yes, (dialog, which) -> {
+                        stop_playing();
+                    }).setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+            dig.setOnDismissListener(dialog -> {
+                stop_playing();
+            });
+        });
+
+
+
     }
 
 
