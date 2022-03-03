a5bd4f6a30e136f6ad1a355f46f249f7410fafa7
ABHISHEK PRALHAD BHALERAO <abhishekbhalerao.appdev@gmail.com>
2022-03-03 15:09:26 +0530

    Encryption

---
+++ app/src/main/java/ab/appdev/drivemaster/AESUtils.java
@@ -0,0 +1,93 @@
+package ab.appdev.drivemaster;
+
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Random;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AESUtils {
+
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+
+    public static void setKey()
+    {
+
+        String myKey ="AlibabaEkkeChalisChor";
+
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String encrypt(String strToEncrypt )
+    {
+        try
+        {
+            setKey();
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String decrypt(String strToDecrypt)
+    {
+        try
+        {
+            setKey();
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static String sizedString(int length) {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = length;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        return(generatedString);
+    }
+
+}
--- app/src/main/java/ab/appdev/drivemaster/MainActivity.java
+++ app/src/main/java/ab/appdev/drivemaster/MainActivity.java
@@ -3,10 +3,12 @@ package ab.appdev.drivemaster;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.FirebaseDatabase;
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     static Information information = new Information();
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,23 +30,24 @@ public class MainActivity extends AppCompatActivity {
 
         sharedpreferences = getSharedPreferences("MyPREFERENCES", Context.MODE_PRIVATE);
 
-        if (sharedpreferences.getString("BroadcastID", "").equals(""))
-            sharedpreferences.edit().putString("BroadcastID", "12234354657685").apply();
+        if (sharedpreferences.getString("BroadcastID", "").equals("")) {
+            sharedpreferences.edit().putString("BroadcastID", AESUtils.encrypt(AESUtils.sizedString(30))).apply();
+        }
 
-        information.setBroadcastId(sharedpreferences.getString("BroadcastID", ""));
-        // information.setBroadcastId("ABHIRAO");
+        if (sharedpreferences.getString(Configurable.SENSITIVITY, "").equals(""))
+            sharedpreferences.edit().putString(Configurable.SENSITIVITY, "5").apply();
+
+
+        information.setBroadcastId(AESUtils.decrypt(sharedpreferences.getString("BroadcastID", "")));
+
 
         FirebaseDatabase.getInstance().getReference("/" + information.getBroadcastId() + "/").child("INFO").setValue("STARTED");
 
         Intent i = new Intent(getApplicationContext(), DriverFaceDetection.class);
 
-        if (sharedpreferences.getString(Configurable.SENSITIVITY, "").equals(""))
-            sharedpreferences.edit().putString(Configurable.SENSITIVITY, "5").apply();
 
         i.putExtra(Configurable.SENSITIVITY, sharedpreferences.getString(Configurable.SENSITIVITY, "0"));
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-
         final Handler handler = new Handler(Looper.getMainLooper());
         handler.postDelayed(() -> {
             startActivity(i);
